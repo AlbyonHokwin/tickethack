2afca6630b48d75bebe739cb633ec40f409a7e95
Camille HAUSTANT <camillehaustant@gmail.com>
2022-11-08 12:01:23 +0100

    route GET for search trips

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -3,6 +3,23 @@ const router = express.Router();
 
 const Trip = require('../models/trips');
 
+router.get('/search/:departure&:arrival&:day', (req, res) => {
+    let {departure, arrival, day} = req.params;
+    day = new Date(day);
+    let nextDay = new Date(day.getTime() + 1000*3600*24);
 
+    Trip.find({
+        departure: {$regex: new RegExp(departure, "i")},
+        arrival: {$regex: new RegExp(arrival, "i")},
+        date: {$gt: day, $lt: nextDay},
+    }).then(trips => {
+        if (trips[0]) {
+            trips.sort((tripA, tripB) => tripA.date - tripB.date);
+            res.json({result: true, trips})
+        } else {
+            res.json({result: false});
+        }
+    });
+});
 
 module.exports = router;
