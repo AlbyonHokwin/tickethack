15ab7e9602c393b1fccc6accb19346431c45673a
Camille HAUSTANT <camillehaustant@gmail.com>
2022-11-09 11:19:27 +0100

    change in css - script allows to display bookings

--- frontend/bookings.js
+++ frontend/bookings.js
@@ -1,3 +1,13 @@
+function timeLeftToStr(date, now) {
+    const deltaT = new Date(date - now);
+
+    if (deltaT.getTime() <= 0) return 'Passed';
+
+    if (deltaT.getTime() < 1000*3600*24) return `Departure in ${deltaT.getHours()} hours`;
+
+    return `Departure in ${Math.floor(deltaT.getTime() / (1000*3600*24))} days`
+}
+
 function emptyBooking() {
     document.querySelector('#bookingContainer').innerHTML =
         `<div id="emptyBooking">
@@ -16,24 +26,39 @@ function filledBookings(bookings) {
         </div>
     `;
 
+    const currentDate = new Date();
+
     for (let booking of bookings) {
         let { departure, arrival, date, price } = booking.trip;
+
         date = new Date(date);
+        console.log(date);
+        const day = `0${date.getDate()}`.slice(-2);
+        const month = `0${date.getMonth() + 1}`.slice(-2);
+        const year = `${date.getFullYear()}`.slice(-2);
+        dateStr = `${month}/${day}/${year}`;
+        const hour = `0${date.getHours()}`.slice(-2);
+        const min = `0${date.getMinutes()}`.slice(-2);
+        hourStr = `${hour}:${min}`;
+
+        const timeLeftStr = timeLeftToStr(date, currentDate);
+
         const newBooking = `
             <div class="bookingRow">
                 <div class="bookingTravel">${departure} > ${arrival}</div>
-                <div class="bookingHour">22:10</div>
+                <div class="bookingDay">${dateStr}</div>
+                <div class="bookingHour">${hourStr}</div>
                 <div class="bookingPrice">${price} €</div>
-                <div class="bookingDeltaTime">Departure in 5 hours</div>
+                <div class="bookingTimeLeft">${timeLeftStr}</div>
             </div>
         `;
+        document.querySelector('#bookingDivider').insertAdjacentHTML('beforebegin', newBooking);
     }
 }
 
 fetch('http://localhost:3000/bookings')
     .then(response => response.json())
     .then(bookings => {
-        console.log(bookings);
-        // if (!bookings.result) emptyBooking();
-        // else filledBookings(bookings.bookings);
+        if (!bookings.result) emptyBooking();
+        else filledBookings(bookings.bookings);
     });
